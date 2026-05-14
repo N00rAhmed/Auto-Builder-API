dea089e9d812b26d1467094d432a170399c648f9
N00rAhmed <noor.ahmed232003@outlook.com>
2026-05-14 04:45:35 +0100

    removed endpoints

--- Auto-Builder-API/Controllers/BoilderplateCodeController.cs
+++ Auto-Builder-API/Controllers/BoilderplateCodeController.cs
@@ -13,55 +13,8 @@ namespace Auto_Builder_API.Controllers
     public class BoilderplateCodeController : ControllerBase
     {
 
-        [HttpGet("hello-world")]
-        public ActionResult GetHello() {
-
-            var template = Template.Parse(@"
-<ul id='products'>
-    <li>
-      <h2>Hello World</h2>
-            NIOFERNIERONFER
-
-
-    </li>
-</ul>
-
-Helloo
-
-
-WORLDDD
-
-");
-            //var result = template.Render(template);
-            // var result = template.Render(new { Products = this.ProductList });
-            //var result = template.Render(new { Name = "World" });
-            //var result = template.Render(template);
-            var result = template.Render();
-
-            return Content(result);
-
-            //return Content("Hello World");
-        }
-
-        [HttpGet("age")]
-        public ActionResult GetAge(int age)
-        {
-            return Content("you are " + $"{age} years old");
-        }
-
-        [HttpGet("template-engine/scriban")]
-        public ActionResult GetTemplateCode()
-        {
-            // Parse a scriban template
-            var template = Template.Parse("print('{{message}}')");
-            var result = template.Render(new { Message = "hello world!" }); // => print('hello world!')
-
-            return Content(result);
-        
-        }
-
         // mongodb+srv://tronn232003_db_user:OGIajsgGJSJZtCB6@mongo-storage.jv1w2i6.mongodb.net/?appName=mongo-storage
-        // retrieve data from mongodb
+        // retrieve params from mongodb
         [HttpGet("mongo-params")]
         public ActionResult GetMongoParams(string uri)
         {
@@ -76,14 +29,6 @@ WORLDDD
         }
 
 
-        [HttpGet("addition")]
-        public ActionResult GetSum(int num1, int num2)
-        {
-            int result = num1 + num2;
-            //return Ok(new { result });
-            //return product == null ? NotFound() : Ok(product);
-            return Ok(result);
-        }
 
         [HttpGet("fastapi-example")]
         public ActionResult FastAPI()
